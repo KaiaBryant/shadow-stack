97199ba7b599e009011e6ab086cd60d9b5da1622
Kaia <kaia.bryant@yahoo.com>
2025-11-25 13:43:45 -0500

    file structure

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Admin from './pages/Admin';
+import Home from './pages/Home';
+import Leaderboard from './pages/Leaderboard';
+import Results from './pages/Results';
+import Simulator from './pages/Simulator';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="/results" element={<Results />} />
+          <Route path="/simulator" element={<Simulator />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Admin.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../styles/Admin.css';
+
+
+export default function Admin() {
+    return (
+        Admin
+    )
+}
