886b1786738844716c14452c165b3c39229ff0e2
Kaia <kaia.bryant@yahoo.com>
2025-11-25 19:34:32 -0500

    Added footer and header template and links

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,45 @@
-function Footer() {
-    return <h1>Footer</h1>;
-}
+import React from "react";
+import "../styles/Footer.css";
+import SocialIcons from "./SocialsIcons";
 
+import InstagramIcon from "../assets/instagram.svg";
+import GitHubIcon from "../assets/github.svg";
+import LinkedInIcon from "../assets/linkedin.svg";
+import TikTokIcon from "../assets/tiktok.svg";
+
+function Footer() {
+    return (
+        <footer className="full-footer-container">
+            <div className="footer-inner-container">
+
+                {/* Social Media Links */}
+                <div className="footer-socials">
+                    <div className="socials-container">
+                        <SocialIcons
+                            href="https://www.instagram.com/"
+                            icon={InstagramIcon}
+                            label="Instagram"
+                        />
+                        <SocialIcons
+                            href="https://github.com/KaiaBryant/shadow-stack"
+                            icon={GitHubIcon}
+                            label="GitHub"
+                        />
+                        <SocialIcons
+                            href="https://twitter.com/"
+                            icon={LinkedInIcon}
+                            label="LinkedIn"
+                        />
+                        <SocialIcons
+                            href="https://www.youtube.com/"
+                            icon={TikTokIcon}
+                            label="TikTok"
+                        />
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+    );
+}
 export default Footer;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Header.css";
+
 function Header() {
-    return <h1>Header
-    </h1>;
+    return (
+        <div className="full-header">
+            <header className="header-container">
+                <nav className="navbar">
+                    <Link to="/simulator">Simulator</Link>
+                    <Link to="/">*Logo*</Link>
+                    <Link to="/leaderboard">Leaderboard</Link>
+                </nav>
+            </header>
+        </div>
+    )
 }
 
 export default Header;
