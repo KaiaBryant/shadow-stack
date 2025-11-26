a325b0fb25eef857bd682aa9e954ae2b442848c4
Kaia <kaia.bryant@yahoo.com>
2025-11-26 12:22:07 -0500

    added pages and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,15 @@ import Home from './pages/Home';
 import Leaderboard from './pages/Leaderboard';
 import Results from './pages/Results';
 import Simulator from './pages/Simulator';
+import CharacterSelect from "./pages/CharacterSelect";
+import CreateUser from "./pages/CreateUsername";
+import GameIntro from "./pages/GameIntro";
+import Login from "./pages/Login";
+import CharacterCard from './components/CharacterCard';
+import ProgressTracker from './components/ProgressTracker';
+import QuestionCard from './components/QuestionCard';
+import ThreatMeter from './components/ThreatMeter';
+
 
 function App() {
   return (
@@ -19,6 +28,10 @@ function App() {
           <Route path="/leaderboard" element={<Leaderboard />} />
           <Route path="/results" element={<Results />} />
           <Route path="/simulator" element={<Simulator />} />
+          <Route path="/CharacterSelect" element={<CharacterSelect />} />
+          <Route path="/CreateUser" element={<CreateUser />} />
+          <Route path="/GameIntro" element={<GameIntro />} />
+          <Route path="/Login" element={<Login />} />
         </Routes>
       </main>
 
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,6 @@
+// Login is the login page for admin access only
+function Login() {
+    return <h1>Login</h1>;
+}
+
+export default Login;
