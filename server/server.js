d717ad22846a27eed9691fbc147b9f5eb6c7659c
Kaia <kaia.bryant@yahoo.com>
2025-11-29 14:55:32 -0500

    local storage for user sign-in

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import questionsRoute from "./routes/questions.js";
 import usersRoute from "./routes/users.js";
 import characterRoutes from "./routes/character.js";
 import leaderboardRoutes from "./routes/leaderboard.js";
-import adminRoutes from "./routes/admin.js";
+// import adminRoutes from "./routes/admin.js";
 
 dotenv.config();
 
@@ -22,7 +22,7 @@ app.use("/api/questions", questionsRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/characters", characterRoutes);
 app.use("/api/leaderboard", leaderboardRoutes);
-app.use("/api/admin", adminRoutes);
+// app.use("/api/admin", adminRoutes);
 
 // --------- START SERVER ---------
 app.listen(PORT, () => {
