9d31e8eb26e4e9951b7b39086a994551a969d966
TehPeh22 <tpeh22@gmail.com>
2025-12-01 22:58:11 -0500

    Updated Simulator.jsx, and CSS global.css Header.css, Footer.css, Home.css and CharacterSelection.css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/Header.css";
 
 function Header() {
+    const location = useLocation();
+
     return (
         <nav className="navbar">
             <div className="container-fluid">
                 <Link to="/" className="navbar-brand">Logo</Link>
                 <div className="ms-auto d-flex gap-5">
-                    <Link to="/" className="nav-link fw-semibold fs-5 text-white">Home</Link>
-                    <Link to="/leaderboard" className="nav-link fw-semibold fs-5 text-white">Leaderboard</Link>
+                    <Link to="/" className={`nav-link fw-semibold fs-5 text-white ${location.pathname === '/' ? 'active' : ''}`}>
+                        Home
+                    </Link>
+                    <Link to="/leaderboard" className={`nav-link fw-semibold fs-5 text-white ${location.pathname === '/leaderboard' ? 'active' : ''}`}>
+                        Leaderboard
+                    </Link>
                 </div>
             </div>
         </nav>
